75f21bc1a5a6dbc58dd26122a6c0413935af7bee
Tanmay Joshi <tanmayjoshi2205@gmail.com>
2021-05-04 16:21:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("yay")
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzjbrIOqh1Ca2M_fu9x5NVKW1AR2iiJN-cpugBJO4mNdpSsRA2lOzBBFBfLkwGDVlH05g/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  console.log("oh", $('.mobile')[0].value.length)
+  if ($('.mobile')[0].value.length == 10) {
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+    console.log("yay", document.getElementsByClassName("mobile")[0].style.display),
+    document.getElementsByClassName("mobile")[0].style.display = "none",
+    document.getElementById("submit-form").innerHTML = "Thanks!",
+  );
+  } else {
+    document.getElementById("submit-form").innerHTML = "Goli beta masti nahi",
+    setTimeout(() => {
+      document.getElementById("submit-form").innerHTML = "Join The Clan"
+    }, 2500);
+  }
+})
+
+$('.mobile').keypress(function(e) {
+    var arr = [];
+    var kk = e.which;
+ 
+    for (i = 48; i < 58; i++)
+        arr.push(i);
+ 
+    if (!(arr.indexOf(kk)>=0))
+        e.preventDefault();
+});
